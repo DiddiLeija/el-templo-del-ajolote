233b645259167ae9fa8e042df99a286b3ba5c398
Diego Ramirez <dr01191115@gmail.com>
2024-09-18 17:41:09 -0600

    Develop a setup file out of `main.py`, fill it with already-known data, and integrate it

--- main.py
+++ main.py
@@ -1,10 +1,35 @@
+import io, json
+
 import pyxel
 
 VERSION = "0.0.1dev1"
 
 
+def get_data(fname: str) -> dict | list:
+    """
+    Tuve la loca idea de meter todos los datos relevantes del
+    resource y los dialogos mientras hacia mods para minecraft,
+    resulta muy practico en casos como este...
+    
+    TODO: Eventualmente migrar a un mejor sistema???
+    """
+    final = dict()
+    with io.open(fname) as dfile:
+        final = json.loads(dfile.read())
+    return final
+
+
+GAME_SETUP = get_data("data.json")
+
+
+class Axel:
+    "a very normal axolotl."
+
+
 class Main:
-    pass
+
+    def __init__(self):
+        self.player = Axel()
 
 
 if __name__ == "__main__":
