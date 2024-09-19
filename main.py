463397d433a158d4e2dcd0dd8b41d6e7488e3ec0
Diego Ramirez <dr01191115@gmail.com>
2024-09-19 12:38:51 -0600

    Introduce more calls to the 'player', treated here as part of the main class

--- main.py
+++ main.py
@@ -6,13 +6,7 @@ VERSION = "0.0.1dev1"
 
 
 def get_data(fname: str) -> dict:
-    """
-    Tuve la loca idea de meter todos los datos relevantes del
-    resource y los dialogos mientras hacia mods para minecraft,
-    resulta muy practico en casos como este...
-    
-    TODO: Eventualmente migrar a un mejor sistema???
-    """
+    # TODO: get a better system
     final = dict()
     try:
         with io.open(fname) as dfile:
@@ -30,6 +24,7 @@ class Main:
     def __init__(self):
         self.x = 960  # 120, 112 -- x8?
         self.y = 896
+        self.player_aspect = ["default", 0]  # to be modified during gameplay
         self.stage = "o"
         pyxel.run(self.update, self.draw)
 
@@ -39,15 +34,22 @@ class Main:
     def draw(self):
         pyxel.cls(0)
         pyxel.camera()
+        draw_x = self.x-64 // 8
+        draw_y = self.y-64 // 8
         if self.stage == "o":
             # overworld
-            draw_x = self.x-64 // 8
-            draw_y = self.y-64 // 8
             # print(draw_x, draw_y)
-            pyxel.bltm(0, 0, 0, draw_x, draw_y, 128, 128)  # TODO: Fixme!
+            pyxel.bltm(0, 0, 0, draw_x, draw_y, 128, 128)
             pyxel.bltm(0, 0, 2, draw_x, draw_y, 128, 128, 0)
+        pyxel.camera(draw_x, draw_y)
+        self.draw_player()
 
     def update_player(self):
+        # TODO: Move on 2.5D!
+        # TODO: "Rotate" the body with aspect fixes
+        pass
+
+    def draw_player(self):
         pass
 
 
