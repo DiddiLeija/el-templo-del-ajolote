21bf0471c51e8d6c3b19f596e1cbb23816575be5
Diego Ramirez <dr01191115@gmail.com>
2024-09-24 17:39:13 -0600

    Fix the collision detector, but bump into other problems during collision fixes

--- main.py
+++ main.py
@@ -20,6 +20,54 @@ def get_tile(tile_x, tile_y, to_list=False):
         return list(pyxel.tilemaps[0].pget(tile_x, tile_y))
     return pyxel.tilemaps[0].pget(tile_x, tile_y)
 
+def detect_collision(x, y):
+    solids = GAME_SETUP["solid_block_tiles"]
+    ns = []
+    for t in solids:
+        ns.append([t[0] // 8, t[1] // 8])
+    solids = ns[:]
+    x1 = x // 8
+    y1 = y // 8
+    x2 = (x + 16 - 1) // 8
+    y2 = (y + 16 - 1) // 8
+    for yi in range(y1, y2+1):
+        for xi in range(x1, x2+1):
+            if get_tile(xi, yi, True) in solids:
+                return True
+    return False
+
+def fix_collision(x, y, dx, dy):
+    # after some failed attempts to run a standalone method,
+    # we've designed this func as a modified version of those
+    # used by "Diddi and Eli" and "Abandon the ship!", along with
+    # "detect_collision" (which went through several modifications too)
+    #abs_dx = abs(dx)
+    #abs_dy = abs(dy)
+    #if abs_dx > abs_dy:
+    #    sign = 1 if dx > 0 else -1
+    #    for _ in range(abs_dx):
+    #        if detect_collision(x + sign, y):
+    #            break
+    #        x += sign
+    #    sign = 1 if dy > 0 else -1
+    #    for _ in range(abs_dy):
+    #        if detect_collision(x, y + sign):
+    #            break
+    #        y += sign
+    #else:
+    #    sign = 1 if dy > 0 else -1
+    #    for _ in range(abs_dy):
+    #        if detect_collision(x, y + sign):
+    #            break
+    #        y += sign
+    #    sign = 1 if dx > 0 else -1
+    #    for _ in range(abs_dx):
+    #        if detect_collision(x + sign, y):
+    #            break
+    #        x += sign
+    return x, y
+    # TODO: fixme!!!
+
 
 GAME_SETUP = get_data("data.json")
 
@@ -50,20 +98,20 @@ class Main:
 
     def update_player(self):
         # movement checks
-        dx, dy = 1, 1
+        dx, dy = 2, 2
         if pyxel.btn(pyxel.KEY_DOWN):
             self.y += 2
             self.player_aspect[0] = "down"
         elif pyxel.btn(pyxel.KEY_UP):
             self.y -= 2
-            dy = -1
+            dy = -2
             self.player_aspect[0] = "up"
         if pyxel.btn(pyxel.KEY_RIGHT):
             self.x += 2
             self.player_aspect[0] = "right"
         elif pyxel.btn(pyxel.KEY_LEFT):
             self.x -= 2
-            dx = -1
+            dx = -2
             self.player_aspect[0] = "left"
         # aspect checks
         if self._clicking_an_arrow([pyxel.KEY_UP, pyxel.KEY_DOWN, pyxel.KEY_LEFT, pyxel.KEY_RIGHT]):
@@ -71,7 +119,7 @@ class Main:
         else:
             self.player_aspect[1] = 0
         # (x, y) corrections according to a solid blocks list
-        self.x, self.y = self._fix_collision(self.x, self.y, dx, dy)
+        self.x, self.y = fix_collision(self.x, self.y, dx, dy)
 
     def draw_player(self):
         img_pick = GAME_SETUP["images"]["axel"][self.player_aspect[0]]
@@ -86,32 +134,6 @@ class Main:
             if pyxel.btn(k):
                 return True
         return False
-    
-    def _fix_collision(self, x: int, y: int, dx: int, dy: int):
-        # a functional collision detector/fixer... -ish?
-        solids = GAME_SETUP["solid_block_tiles"]
-        ns = []
-        for t in solids:
-            ns.append([t[0] // 8, t[1] // 8])
-        solids = ns[:]
-        x1 = x // 8
-        y1 = y // 8
-        x2 = (x + 8 - 1) // 8
-        y2 = (y + 8 - 1) // 8
-        # print(x1, y1, "---", x2, y2)
-        #print(list(get_tile(x1, y1)), list(get_tile(x1, y1)) in solids)
-        rx = range(x1-8, x2+1) if dx < 0 else range(x1, x2)
-        ry = range(y1-8, y2+1) if dy < 0 else range(y1, y2)
-        for yi in range(y1, y2+1):
-            for xi in range(x1, x2+1):
-                if get_tile(xi, yi, True) in solids:
-                    # print("solid")
-                    pass  # TODO: fixme
-        #for yi in range(y1, y2 + 1):
-        #    for xi in range(x1, x2 + 1):
-        #        if get_tile(xi, yi) in solids:
-        #            # fix!
-        return x, y
 
 if __name__ == "__main__":
     pyxel.init(128, 128, title=f"El Templo del Ajolote v{VERSION}", capture_sec=120)
